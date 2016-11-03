1a34ccbf73f0ce2dac2a3f3b35fe0846fbd5dac0
Whitney Cormack <cormack.whitney@gmail.com>
2016-11-02 23:25:31 -0500

    growTree function working

--- quiz.js
+++ quiz.js
@@ -1,47 +1,45 @@
-// declare vars
-
 var height = document.getElementById('height');
 var character = document.getElementById('character');
 var button = document.getElementById('button');
+var reset = document.getElementById('reset');
 
-button.addEventListener("click", checkInput);
-
-function checkInput() {
+function checkInput(tree) {
   if (!height.value || !character.value) {
     alert("You must enter a value!");
   } else {
-      var tree = {
-        height: height.value,
-        character: character.value
-      }
-      console.log(tree);
     growTree(tree);
   }
-}
+};
 
-function growTree(tree) {
+function growTree() {
+  var tree = {
+    height: height.value,
+    character: character.value
+    }
   var treeString = "";
-  var inputHeight = tree.height;
-  var inputChar = tree.character;
-  var numSpaces = inputHeight - i;
-  var numChar = (2 * i) - 1;
-
-  for(var i = 0; i < inputHeight; i++) {
-    treeString += tree[i];
-
-    console.log("inside for loop", treeString);
-
-
-    // if () {
-    //   treeString += " ";
-    // }
-    // console.log("tree", growTree(tree));
+  var space = " ";
+  for (var i = 0; i < tree.height; i++) {
+    console.log("testing", space.repeat((tree.height - 1) - i) + tree.character.repeat((i * 2) + 1));
   }
+};
+
+function EnterKey(event) {
+  if (event.keyCode === 13) {
+    checkInput();
+  }
+};
+
+function Reset() {
+  height.value = "";
+  character.value = "";
+  console.clear();
 }
 
-// growTree(tree);
+button.addEventListener("click", checkInput);
+height.addEventListener("keypress", EnterKey);
+character.addEventListener("keypress", EnterKey);
+reset.addEventListener("click", Reset);
 
-
-
+//
 
 
